8394653e72f61fbf9d93e88aab1125cead62e359
Danny Willems <danny@b2c2.net>
2017-03-26 14:34:04 +0200

    Use Dsubml as basis grammar.

--- src/grammar.cppo.ml
+++ src/grammar.cppo.ml
@@ -4,76 +4,96 @@ open BindingForms
 (* ------------------------------------------------------ *)
 (* Types of termes and types of the DOT calculus *)
 
-type method_label = string[@opaque]
+type field_label = string[@opaque]
 
 and type_label = string[@opaque]
 
-(* Terms *)
 and ('bn, 'fn) term =
-  (* Variable *)
-  | TermVar of 'fn
-  (* {x => t^{x}} *)
-  | TermRecursiveRecord of ('bn, ('bn, 'fn) decl list) abs
-  (* t.m(t) *)
-  | TermMethodApp of ('bn, 'fn) term * method_label * ('bn, 'fn) term
-  (* | TermMethodApp of 'fn * method_label * ('bn, 'fn) term *)
+  (* x *)
+  | TermVariable of 'fn
+  (* λ(x : S) t --> (S, (x, t)) *)
+  | TermAbstraction of
+      ('bn, 'fn) typ * ('bn, ('bn, 'fn) term) abs
+  (* x y *)
+  | TermVarApplication of 'fn * 'fn
+  (* let x = t in u --> (t, (x, u))*)
+  | TermLet of ('bn, 'fn) term * ('bn, ('bn, 'fn) term) abs
 
-(* Objects.
-   TODO: Replace list with a HashMap for efficiency.
-*)
+  (* ----- Unofficial terms ----- *)
+  (* t : T *)
+  | TermAscription of ('bn, 'fn) term * ('bn, 'fn) typ
+  (* Add the unimplemented term allows to define terms without given their implementation. Useful for testing. *)
+  | TermUnimplemented
+
+  (* ----- Beginning of DOT terms ----- *)
+  (* {x => t^{x}} *)
+  | TermRecursiveRecord of ('bn, ('bn, 'fn) decl) abs
+  (* x.a *)
+  | TermFieldSelection of 'fn * string
+
+(* Objects. Type tag becomes DeclarationType (instead of TermTypeTag) *)
 and ('bn, 'fn) decl =
   (* L = T *)
   | DeclarationType of type_label * ('bn, 'fn) typ
-  (* m(x : S) = t *)
-  | DeclarationMethod of method_label * ('bn, 'fn) typ * ('bn, ('bn, 'fn) term) abs
+  (* a = t *)
+  | DeclarationField of field_label * ('bn, 'fn) term
+  (* d ∧ d' *)
+  | DeclarationAggregate of ('bn, 'fn) decl * ('bn, 'fn) decl
 
-(* Types *)
 and ('bn, 'fn) typ =
   (* Top type : ⊤ *)
   | TypeTop
   (* Bottom type : ⟂ *)
   | TypeBottom
+  (* { L : S..T } --> (L, S, T) *)
+  | TypeDeclaration of type_label * ('bn, 'fn) typ * ('bn, 'fn) typ
+  (* x.L *)
+  | TypeProjection of 'fn * type_label
+  (* ∀(x : S) T --> (S, (x, T)) *)
+  | TypeDependentFunction of
+      ('bn, 'fn) typ *
+      ('bn, ('bn, 'fn) typ) abs
+  (* ----- Beginning of DOT types ----- *)
   (* T ∧ T *)
   | TypeIntersection of ('bn, 'fn) typ * ('bn, 'fn) typ
-  (* T ∨ T *)
-  | TypeUnion of ('bn, 'fn) typ * ('bn, 'fn) typ
   (* { z => T^{z} } *)
   | TypeRecursive of ('bn, ('bn, 'fn) typ) abs
-  (* L : S..T *)
-  | TypeTypeMember of type_label * ('bn, 'fn) typ * ('bn, 'fn) typ
-  (* m(x : S) : T *)
-  | TypeMethodMember of method_label * ('bn, 'fn) typ * ('bn, ('bn, 'fn) typ) abs
-  (* x.L *)
-  | TypePathDependent of 'fn * type_label
+  (* { a : T } *)
+  | TypeFieldDeclaration of field_label * ('bn, 'fn) typ
 
+and ('bn, 'fn) top_level =
+  | TopLevelLet of 'fn * ('bn, 'fn) term
+  | TopLevelTerm of ('bn, 'fn) term
 
 [@@deriving
   visitors {
-    variety = "iter";
-    ancestors = ["BindingForms.iter"]
-  },
-  visitors {
-    variety = "map";
+    variety = "map" ;
     ancestors = ["BindingForms.map"]
   },
   visitors {
-    variety = "iter2";
+    variety = "iter" ;
+    ancestors = ["BindingForms.iter"]
+  },
+  visitors {
+    variety = "iter2" ;
     ancestors = ["BindingForms.iter2"]
   },
   visitors {
-    variety = "endo";
+    variety = "endo" ;
     ancestors = ["BindingForms.endo"]
   }
-
 ]
+
 
 type raw_term = (string, string) term
 type raw_typ = (string, string) typ
 type raw_decl = (string, string) decl
+type raw_top_level = (string, string) top_level
 
 type nominal_term = (Atom.t, Atom.t) term
 type nominal_typ = (Atom.t, Atom.t) typ
 type nominal_decl = (Atom.t, Atom.t) decl
+type nominal_top_level = (Atom.t, Atom.t) top_level
 
 #include "AlphaLibMacros.cppo.ml"
 
@@ -81,3 +101,4 @@ __ALL
 ALL(term)
 ALL(decl)
 ALL(typ)
+ALL(top_level)
