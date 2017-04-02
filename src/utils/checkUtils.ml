20cb9728f5aad4ffd80ca899b7a396d1a39ba4a2
Danny Willems <contact@danny-willems.be>
2017-04-02 22:25:03 +0200

    Check function for labels.

--- src/utils/checkUtils.ml
+++ src/utils/checkUtils.ml
@@ -4,54 +4,48 @@ let check_well_formedness context typ =
 
 let check_avoidance_problem x s =
   if Grammar.occurs_typ x s
-  then raise (Error.AvoidanceProblem(
-      (Printf.sprintf
-         "%s appears in %s."
-         (AlphaLib.Atom.show x)
-         (Print.string_of_nominal_typ s)
-      ),
-      x,
-      s
-    ))
+  then Error.raise_avoidance_problem x s
 
 let check_type_match context term s t =
   if not (Subtype.is_subtype ~context s t)
-  then raise
-      (Error.TypeMismatch (
-          Printf.sprintf
-            "ALL-E: %s must be a subtype of %s but it's of type %s."
-            (Print.string_of_nominal_term term)
-            (Print.string_of_raw_typ (Grammar.show_typ s))
-            (Print.string_of_raw_typ (Grammar.show_typ t)),
-          (s, t)
-          )
-        )
+  then Error.raise_type_mismatch term s t
 
 let check_subtype context s t =
   if not (Subtype.is_subtype ~context s t)
-  then (
-    let str =
-      Printf.sprintf
-        "%s is not a subtype of %s"
-        (Print.string_of_nominal_typ s)
-        (Print.string_of_nominal_typ t)
-    in
-    raise (Error.Subtype (str, s, t))
-  )
+  then Error.raise_subtype s t
 
 let check_disjoint_domains d1 d2 =
-  let domain_d1 = TypeUtils.extract_label_from_declaration d1 in
-  let domain_d2 = TypeUtils.extract_label_from_declaration d2 in
+  let domain_d1 = TypeUtils.labels_of_declaration d1 in
+  let domain_d2 = TypeUtils.labels_of_declaration d2 in
   if not (
       TypeUtils.SetFieldDeclaration.is_empty (
         TypeUtils.SetFieldDeclaration.inter domain_d1 domain_d2
       )
     )
-  then raise (
-      Error.AggregationIntersectionNotEmpty(
-        "When defining an aggregation, the domains must be disjoint.",
-        d1,
-        d2
-      ))
+  then Error.raise_aggregate_intersection_not_empty d1 d2
 
+(* Get labels from a declaration *)
+let check_record_contains_label record label =
+  let labels_of_record =
+    TypeUtils.labels_of_declaration record
+  in
+  TypeUtils.SetFieldDeclaration.mem label labels_of_record
 
+let check_record_contains_field =
+  check_record_contains_label
+
+let check_record_contains_type =
+  check_record_contains_label
+
+(* Get labels from a type *)
+let check_type_contains_label context recursive_type field_label =
+  let labels_of_recursive_type =
+    TypeUtils.labels_of_recursive_type context recursive_type
+  in
+  TypeUtils.SetFieldDeclaration.mem field_label labels_of_recursive_type
+
+let check_type_contains_type =
+  check_type_contains_label
+
+let check_type_contains_field =
+  check_type_contains_label
--- src/utils/checkUtils.mli
+++ src/utils/checkUtils.mli
@@ -25,3 +25,31 @@ val check_disjoint_domains :
   Grammar.nominal_decl ->
   Grammar.nominal_decl ->
   unit
+
+val check_record_contains_field :
+  Grammar.nominal_decl ->
+  Grammar.field_label ->
+  bool
+
+val check_record_contains_type :
+  Grammar.nominal_decl ->
+  Grammar.type_label ->
+  bool
+
+val check_type_contains_label :
+  ContextType.context ->
+  Grammar.nominal_typ ->
+  string ->
+  bool
+
+val check_type_contains_field :
+  ContextType.context ->
+  Grammar.nominal_typ ->
+  string ->
+  bool
+
+val check_type_contains_type :
+  ContextType.context ->
+  Grammar.nominal_typ ->
+  string ->
+  bool
